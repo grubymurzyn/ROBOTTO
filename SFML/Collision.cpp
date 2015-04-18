c2452244037ef2787596886bc37f85853d98c0bd
grubymurzyn@gmail.com <malutki150894>
2015-04-19 00:34:52 +0200

    Commit test2

--- SFML/Collision.cpp
+++ SFML/Collision.cpp
@@ -11,11 +11,16 @@ Collision::~Collision()
 }
 
 bool Collision::checkIfCollision(RectangleShape player, vector<RectangleShape> object_list){
+	
 
 	for each (RectangleShape object in object_list)
-	{
+	{	
+
+
 		if (player.getGlobalBounds().intersects(object.getGlobalBounds()))return true;
+
 			
 	}
-	return false;
+	return 0;
 }
+
--- SFML/Engine.cpp
+++ SFML/Engine.cpp
@@ -11,13 +11,13 @@ Engine::~Engine(void)
 }
 void Engine::runEngine(RenderWindow &window)
 {
-	bool menu = false;
+	bool menu = false, top = false, bottom = false,left=false,right=false;
 	Collision collision;
 	int counter =0;
 	float t = 0;
 	Clock clock;
 	const float gravity = 1;
-	int groundHeight = 400, maxHeight = 150;
+	int groundHeight = 400, maxHeight = 150,actualHeight;
 	static Vector2f velocity(Vector2f (0,0)),position;
 	float jumpSpeed = 7.1f,moveSpeed = 100.1f;
 	RectangleShape rect(Vector2f(20,20));
@@ -26,40 +26,46 @@ void Engine::runEngine(RenderWindow &window)
 	vector<RectangleShape> obstacle_list;
 
 	RectangleShape obstacle1(Vector2f(20, 20));
-	obstacle1.setPosition(300, 300);
+	obstacle1.setPosition(400, 380);
 	obstacle1.setFillColor(Color::Red);
 	obstacle_list.push_back(obstacle1);
 	while (!menu)
 	{
+				float delta = clock.getElapsedTime().asSeconds() - t;
+				t = clock.getElapsedTime().asSeconds();
 
 			if (Keyboard::isKeyPressed(Keyboard::Right)) {
-				velocity.x = moveSpeed;
-				if (Keyboard::isKeyPressed(Keyboard::Space) && counter <maxHeight){
-					velocity.y -= jumpSpeed;
-					++counter;
-				}
+					right = false;
+					velocity.x = moveSpeed;
+					if (Keyboard::isKeyPressed(Keyboard::Space) && counter < maxHeight){
+						velocity.y -= jumpSpeed;
+						++counter;
+					}
+					if (collision.checkIfCollision(rect, obstacle_list)) right = true;
 			}
 			else if (Keyboard::isKeyPressed(Keyboard::Left)) {
-				velocity.x = -moveSpeed;
-				if (Keyboard::isKeyPressed(Keyboard::Space) && counter <maxHeight){
-					velocity.y -= jumpSpeed;
-					++counter;
-				}
+					left = false;
+					velocity.x = -moveSpeed;
+					if (Keyboard::isKeyPressed(Keyboard::Space) && counter < maxHeight){
+						velocity.y -= jumpSpeed;
+						++counter;
+					}
+					if (collision.checkIfCollision(rect, obstacle_list)) left = true;
 			}
-			else if ((Keyboard::isKeyPressed(Keyboard::Space)) && counter <maxHeight){
-					velocity.y -= jumpSpeed;
-					++counter;
-					
+			else if ((Keyboard::isKeyPressed(Keyboard::Space)) && counter < maxHeight){
+
+				velocity.y -= jumpSpeed;
+				++counter;
 			}
 			else{
 				velocity.x = 0;
 			}
+			if ((rect.getPosition().y + rect.getSize().y < groundHeight || velocity.y < 0)){
 
-			if (rect.getPosition().y + rect.getSize().y < groundHeight || velocity.y <0){
-				
 				velocity.y += gravity;
 				++counter;
 			}
+
 			else
 			{
 
@@ -68,13 +74,10 @@ void Engine::runEngine(RenderWindow &window)
 				velocity.y = 0;
 			}
 
-			float delta = clock.getElapsedTime().asSeconds() - t;
-			t = clock.getElapsedTime().asSeconds();
-			std::cout <<"COLLISION: "<<collision.checkIfCollision(rect,obstacle_list)<< endl;
-			std::cout << "COUNTER: " << counter << endl;
-			rect.move(velocity.x*delta, velocity.y*delta);
+			/*std::cout <<"COLLISION: "<<collision.checkIfCollision(rect,obstacle_list)<< endl;*/
+			//std::cout << "POZYCJA: " << rect.getPosition().y + rect.getSize().y <<" "<<rect.getPosition().x<< endl;
+				rect.move(velocity.x*delta, velocity.y*delta);
 
-	
 			
 		//KOLIZJA
 			window.clear(Color(0,240,255));
